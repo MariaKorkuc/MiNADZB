198ea081e58fc9c644ccb41e7e26d06cb531c673
Maria Korkuc <73887730+MariaKorkuc@users.noreply.github.com>
2021-11-28 13:03:53 +0100

    Update program.py

--- Scrapping/DatasetScrap/DatasetScrap/program.py
+++ Scrapping/DatasetScrap/DatasetScrap/program.py
@@ -5,7 +5,6 @@ from scrapy.selector import Selector
 import requests as requests
 import os
 from scrapy.crawler import CrawlerProcess
-#def scrap():
 
 
 
@@ -18,9 +17,7 @@ def set_local_db(username, password):
     if db_name not in database_list:
         db_local = local[db_name]
         records = db_local[coll_name]
-        # process = CrawlerProcess()
-        # process.crawl(DatasetSpider)
-        # process.start()
+        
         response = requests.get('https://github.com/nytimes/covid-19-data/blob/master/live/us-counties.csv')
         rows = Selector(response).xpath("//table[@class='js-csv-data csv-data js-file-line-container']//tbody//tr")
         for row in rows:
@@ -40,32 +37,19 @@ def set_local_db(username, password):
 
     set_cloud(records, coll_name, db_name, username, password)
 
-    # db_local = local['MINADZB']
-    # db_local.drop_collection(coll_name)
-
 
 
 def set_cloud(records, coll_name, db_name, username, password):
     link = "mongodb+srv://"+username+":"+password+"@covid.7imo3.mongodb.net/MINADZB_cloud?retryWrites=true&w=majority"
     client = MongoClient(link)
-    # client = MongoClient(
-    #     "mongodb+srv://admin:Password123@covid.7imo3.mongodb.net/MINADZB_cloud?retryWrites=true&w=majority")
-    # client = MongoClient(
-    #     "mongodb://admin:Password123@covid-shard-00-00.7imo3.mongodb.net:27017,covid-shard-00-01.7imo3.mongodb.net:27017,covid-shard-00-02.7imo3.mongodb.net:27017/MINADZB_cloud?ssl=true&replicaSet=atlas-gghftr-shard-0&authSource=admin&retryWrites=true&w=majority")
-
+    
     db = client[db_name]
     db.drop_collection(coll_name)
     records_cloud = db[coll_name]
 
     for record in records.find({}):
         records_cloud.insert_one(record)
-    # db = client.get_default_database('MINADZB_cloud')
-    # records = db.covid_records
-    # client.admin.command('copydb',
-    #                      fromdb='MINADZB',
-    #                      todb='MINADZB_cloud',
-    #                      fromhost="localhost:27018")
-
+        
 
 if __name__ == '__main__':
     print('COVID DATABASE\n')
