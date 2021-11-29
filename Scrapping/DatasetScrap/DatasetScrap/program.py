74efe874cdcd06aab86e563fd177c1b96e8299ee
piotrjasiu95 <piotrjasiu95@gmail.com>
2021-11-29 15:00:45 +0100

    update

--- Scrapping/DatasetScrap/DatasetScrap/program.py
+++ Scrapping/DatasetScrap/DatasetScrap/program.py
@@ -15,18 +15,17 @@ def set_local_db(username, password):
     coll_name = 'covid'
     database_list = local.list_database_names()
 
-    if db_name not in database_list:
-        db_local = local[db_name]
-        records = db_local[coll_name]
+    # if db_name not in database_list:
+    db_local = local[db_name]
+    records = db_local[coll_name]
         # process = CrawlerProcess()
         # process.crawl(DatasetSpider)
         # process.start()
-        #1
 
-        response = requests.get('https://github.com/nytimes/covid-19-data/blob/master/live/us-counties.csv')
-        rows = Selector(response).xpath("//table[@class='js-csv-data csv-data js-file-line-container']//tbody//tr")
-        for row in rows:
-            new_item = {
+    response = requests.get('https://github.com/nytimes/covid-19-data/blob/master/live/us-counties.csv')
+    rows = Selector(response).xpath("//table[@class='js-csv-data csv-data js-file-line-container']//tbody//tr")
+    for row in rows:
+        new_item = {
                 'date': row.xpath('td[2]//text()').extract_first(),
                 'county': row.xpath('td[3]//text()').extract_first(),
                 'state': row.xpath('td[4]//text()').extract_first(),
@@ -34,11 +33,18 @@ def set_local_db(username, password):
                 'deaths': row.xpath('td[7]//text()').extract_first(),
                 'confirmed_cases': row.xpath('td[8]//text()').extract_first(),
                 'confirmed_deaths': row.xpath('td[9]//text()').extract_first()
-            }
-            records.insert_one(new_item)
-    else:
-        db_local = local[db_name]
-        records = db_local[coll_name]
+        }
+
+        #if(records.countDocuments({'date': new_item.get('date')})>=3251):
+        #if (records.find({'date': new_item.get('date')}).countDocuments() > 3251):
+        if(records.find({'date': new_item.get('date')}).count() >= 3251):
+            print("Today's data is already in DB")
+            exit(0)
+
+        records.insert_one(new_item)
+    # else:
+    #     db_local = local[db_name]
+    #     records = db_local[coll_name]
 
     set_cloud(records, coll_name, db_name, username, password)
 
